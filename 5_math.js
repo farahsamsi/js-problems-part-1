9996661347ec31b1df6988c875730cb0e57d718d
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-08 14:50:23 +0600

    math keyword related syntax

---
+++ 5_math.js
@@ -0,0 +1,21 @@
+const min = Math.min(45,1,56,65,82);
+console.log('Min', min);
+
+const max = Math.max(45,1,56,65,82);
+console.log('Max', max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-10));
+
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.85));
+console.log(Math.floor(2.85));
+console.log(Math.ceil(2.85));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
